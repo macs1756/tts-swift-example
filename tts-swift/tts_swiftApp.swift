9f9a0ee8986dfed272bd5f3a77d3943764fadadb
macs1756 <kindrativmaksum312@gmail.com>
2024-10-18 21:24:14 +0300

    Initial Commit

---
+++ tts-swift/tts_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  tts_swiftApp.swift
+//  tts-swift
+//
+//  Created by user on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct tts_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
